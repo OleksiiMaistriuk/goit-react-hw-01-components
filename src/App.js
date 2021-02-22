7b4f7826ca188b6adbeb7aa18ba61cede7e2c654
OleksiiMaistriuk <65893898+OleksiiMaistriuk@users.noreply.github.com>
2021-02-22 18:54:08 +0100

    добавил папку component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import FriendList from './FriendList/index.js';
-import Profile from './SocialNetworkProfile/index.js';
-import Statistic from './StatisticSection/index.js';
-import TransactionHistory from './TransactionHistory/index.js';
+import FriendList from './components/FriendList/index.js';
+import Profile from './components/SocialNetworkProfile/index.js';
+import Statistic from './components/StatisticSection/index.js';
+import TransactionHistory from './components/TransactionHistory/index.js';
 
-import user from './SocialNetworkProfile/user.json';
-import statisticalData from './StatisticSection/statistical-data.json';
-import friends from './FriendList/friends.json';
-import transactions from './TransactionHistory/transactions.json';
+import user from './components/SocialNetworkProfile/user.json';
+import statisticalData from './components/StatisticSection/statistical-data.json';
+import friends from './components/FriendList/friends.json';
+import transactions from './components/TransactionHistory/transactions.json';
 
 const App = () => {
   return (
